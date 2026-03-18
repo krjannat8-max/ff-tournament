0a152d8b603f8654db691f139a4e78a43ab662f2
Your Name <your-email@example.com>
2026-03-18 10:09:15 -0700

    Updated Branding and Added Particle Effects

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-pro-v2'; // Increment this whenever you update the app
+const CACHE_NAME = 'ff-pro-v3'; // Increment this whenever you update the app
 const ASSETS = [
     './',
     './index.html',
