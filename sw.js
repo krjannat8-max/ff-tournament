6e34e4a37d8d12bd229ea99df9b69fa95c1b02c4
Your Name <your-email@example.com>
2026-03-18 10:24:51 -0700

    Added SEO meta tags and Download App button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-pro-v6'; // Increment this whenever you update the app
+const CACHE_NAME = 'ff-pro-v7'; // Increment this whenever you update the app
 const ASSETS = [
     './',
     './index.html',
